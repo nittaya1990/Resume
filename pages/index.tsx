127dc721f7e693eaeb4ccde570764a06bb4c306d
elocremarc <42568538+elocremarc@users.noreply.github.com>
2021-06-10 13:33:11 -0700

    Nextjs Chackra conversion initial

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+
+import {Box,Center} from "@chakra-ui/react"
+export default function Home() {
+  return (<>
+  <Center  bg="blue.500" color="white"  borderRadius="lg" p={5} m={2} > This is a box </Center> </>
+    
+  )
+
+}
