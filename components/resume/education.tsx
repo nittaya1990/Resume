cec6ef2f7576b4a888079149c1b604a4abfbc24b
elocremarc <42568538+elocremarc@users.noreply.github.com>
2021-06-10 21:49:59 -0700

    Project render maping

--- components/resume/education.tsx
+++ components/resume/education.tsx
@@ -1,25 +1,26 @@
 import React from "react";
-import { Text, Container, Center } from "@chakra-ui/react"
-
+import { Text, Container, Center } from "@chakra-ui/react";
 
 export default function Education() {
-    return (<>   <Center textTransform="uppercase"
-        fontWeight="bold" fontsize="64" >
-
+  return (
+    <>
+      {" "}
+      <Center textTransform="uppercase" fontWeight="bold" fontsize="64">
         <Text
-            bgGradient="linear(to-l, #7928CA,#FF0080)"
-            bgClip="text"
-            fontSize="2xl"
-            fontWeight="extrabold"
+          bgGradient="linear(to-l, #7928CA,#FF0080)"
+          bgClip="text"
+          fontSize="2xl"
+          fontWeight="extrabold"
         >
-            Education
+          Education
         </Text>
+      </Center>
+      <Container rounded="lg" bg="#7979793b" m="2" p={2} w={500}>
+        <Text pl={4}>Manufacturing Engineering Technology CAD/CAM (ABET)</Text>
+        <Text pl="4">Bachelor of Science</Text>
 
-    </Center>
-        <Container rounded='lg' bg="#7979793b" m="2" p={2} w={500} ><Text pl={4}>Manufacturing Engineering Technology CAD/CAM (ABET)
-
-        </Text><Text pl="4">Bachelor of Science</Text>
-
-            <Text pl="4">Western Washington University, Bellingham, WA 2014
-            </Text></Container></>)
+        <Text pl="4">Western Washington University, Bellingham, WA 2014</Text>
+      </Container>
+    </>
+  );
 }
