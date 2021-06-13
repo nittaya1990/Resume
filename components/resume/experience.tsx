f04c546faf25393846f967b1357e03eb28f61014
elocremarc <42568538+elocremarc@users.noreply.github.com>
2021-06-12 18:10:59 -0700

    fixed width change

--- components/resume/experience.tsx
+++ components/resume/experience.tsx
@@ -18,16 +18,16 @@ export default function Experience() {
         <Box rounded="lg" bg="#7979793b" m="2" p={2}>
           <Container fontWeight="bold">Kineto Films, Co-owner CEO:</Container>
           <Text pl="4">Jan 2017 – March 2020</Text>
-          <Text color="white" pl="4" w={400}>
+          <Text color="white" pl="4">
             Kineto Films specialized in high quality video, content marketing
             and storytelling. It helped its clients grow and thrive in a social
             media connected world.
           </Text>
         </Box>
-        <Box rounded="lg" bg="#7979793b" m="2" p={4}>
+        <Box rounded="lg" bg="#a3a3a33a" m="2" p={4}>
           <Container fontWeight="bold"> Polyform US, Manufacturing </Container>
           <Text pl="4">Engineer: March 2015 – Dec 2017</Text>
-          <Container pl="4" w={500}>
+          <Container pl="4">
             Created complex kinematic assemblies of rotomolding machines.
             Designed a new production line that included oversight for the
             entire project. Managed multiple projects for instance a
