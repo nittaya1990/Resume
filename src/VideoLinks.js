49551c3671060178f3c806d7d28a21937d2a054b
elocremarc <42568538+elocremarc@users.noreply.github.com>
2021-06-17 19:51:19 -0700

    Merge Main

--- src/VideoLinks.js
+++
@@ -1,8 +0,0 @@
-const links = [
-  {
-    id: "bafybeic43eip3hgvasyhkyyavqsiiayiy4d45c244snrl35cohdxzfc744",
-    url:
-      "https://bafybeidep46i73cnjyztxljqa3scywc6fqbkziyk2g65mupyjx6n7njtzy.ipfs.dweb.link/",
-  },
-];
-export { links };
